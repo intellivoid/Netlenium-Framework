b325a8b5c74c5194398945d7bf83b48bcad35bc6
Narrakas <zixing.narrakas1996@gmail.com>
2019-03-05 18:13:48 -0300

    Upgraded from GeckoFX-45 to GeckoFX-60.0 (32 Bits)

--- Netlenium.Driver.GeckoFXLib/Element.cs
+++ Netlenium.Driver.GeckoFXLib/Element.cs
@@ -5,6 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gecko.WebIDL;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using Gecko.DOM;
 
 namespace Netlenium.Driver.GeckoFXLib
 {
@@ -48,7 +53,45 @@ namespace Netlenium.Driver.GeckoFXLib
         public void Click()
         {
             GeckoHtmlElement GeckoHTMLElement = (GeckoHtmlElement)_Element;
+
             GeckoHTMLElement.Click();
+            //DomEventArgs ev = _DriverController._GeckoWebBrowser.Document.CreateEvent("MouseEvent");
+            //var webEvent = new Event(_DriverController._GeckoWebBrowser.Window.DomWindow, ev.DomEvent as nsISupports);
+
+            //webEvent.InitEvent("mousedown", true, true);
+            //_Element.GetEventTarget().DispatchEvent(ev);
+
+            //webEvent.InitEvent("mouseup", true, true);
+            //_Element.GetEventTarget().DispatchEvent(ev);
+
+            //webEvent.InitEvent("click", true, true);
+            //_Element.GetEventTarget().DispatchEvent(ev);
+        }
+
+        private void SendKeyEvent(string type, string key, bool alt, bool ctrl, bool shift)
+        {
+            var WebBrowser = _DriverController._GeckoWebBrowser;
+            // Escape for JS.
+            key = key.Replace("\\", "\\\\");
+            var instance = Xpcom.CreateInstance<nsITextInputProcessor>("@mozilla.org/text-input-processor;1");
+            using (var context = new AutoJSContext(_DriverController._GeckoWebBrowser.Window))
+            {
+                var result = context.EvaluateScript(
+                    $@"new KeyboardEvent('', {{ key: '{key}', code: '', keyCode: 0, ctrlKey : {(ctrl ? "true" : "false")}, shiftKey : {(shift ? "true" : "false")}, altKey : {(alt ? "true" : "false")} }});");
+                instance.BeginInputTransaction((mozIDOMWindow)((GeckoWebBrowser)WebBrowser).Document.DefaultView.DomWindow, new TestCallback());
+                instance.Keydown((nsIDOMEvent)result.ToObject(), 0, 1);
+                instance.Keyup((nsIDOMEvent)result.ToObject(), 0, 1);
+            }
+
+            Marshal.ReleaseComObject(instance);
+        }
+        public class TestCallback : nsITextInputProcessorCallback
+        {
+            public bool OnNotify(nsITextInputProcessor aTextInputProcessor, nsITextInputProcessorNotification aNotification)
+            {
+                Console.WriteLine("TestCallback:OnNotify");
+                return true;
+            }
         }
 
         /// <summary>
