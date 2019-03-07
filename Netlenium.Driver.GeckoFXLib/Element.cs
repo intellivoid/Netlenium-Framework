cf0175506cd99cd2a6f0e268b4690ea5aedf4087
Narrakas <zixing.narrakas1996@gmail.com>
2019-03-07 19:20:18 -0300

    Removed unused references

--- Netlenium.Driver.GeckoFXLib/Element.cs
+++ Netlenium.Driver.GeckoFXLib/Element.cs
@@ -2,14 +2,7 @@
 using Gecko.WebIDL;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Gecko.WebIDL;
-using System.Windows.Forms;
-using System.IO;
 using System.Runtime.InteropServices;
-using Gecko.DOM;
 using System.Diagnostics;
 
 namespace Netlenium.Driver.GeckoFXLib
