35131691e8f766d59abefcff9806604f10e3fbad
narrakas <zixing.narrakas1996@gmail.com>
2019-03-13 21:51:04 -0300

    Updated Linux Make files to use msbuild instead of the deprecated xbuild

--- linux_make_release.sh
+++ linux_make_release.sh
@@ -1,22 +1,22 @@
-gecko_linux="packages/Geckofx60.32.Linux.60.0.26/lib/net40"
+#gecko_linux="packages/Geckofx60.32.Linux.60.0.26/lib/net40"
 
 echo "restoring nuget packages"
 nuget restore "Netlenium Framework.sln"
 
-echo "running xbuild"
-xbuild /p:Configuration=LinuxRelease "Netlenium Framework.sln"
+echo "running msbuild"
+msbuild /p:Configuration=LinuxRelease "Netlenium Framework.sln"
 
-echo "removing win32 geckofx-core"
-rm bin/LinuxRelease/Geckofx-Core.dll
-echo "removing win32 geckofx-winforms"
-rm bin/LinuxRelease/Geckofx-Winforms.dll
+#echo "removing win32 geckofx-core"
+#rm bin/LinuxRelease/Geckofx-Core.dll
+#echo "removing win32 geckofx-winforms"
+#rm bin/LinuxRelease/Geckofx-Winforms.dll
 
-echo "adding linux geckofx-core 60.0.26 32bit (from nuget)"
-cp $gecko_linux/Geckofx-Core.dll bin/LinuxRelease/Geckofx-Core.dll
-cp $gecko_linux/Geckofx-Core.dll.mdb bin/LinuxRelease/Geckofx-Core.dll.mdb
+#echo "adding linux geckofx-core 60.0.26 32bit (from nuget)"
+#cp $gecko_linux/Geckofx-Core.dll bin/LinuxRelease/Geckofx-Core.dll
+#cp $gecko_linux/Geckofx-Core.dll.mdb bin/LinuxRelease/Geckofx-Core.dll.mdb
 
-echo "adding linux geckofx-winforms 60.0.26 32bit (from nuget)"
-cp $gecko_linux/Geckofx-Winforms.dll bin/LinuxRelease/Geckofx-Winforms.dll
-cp $gecko_linux/Geckofx-Winforms.dll.mdb bin/LinuxRelease/geckofx-Winforms.dll.mdb
+#echo "adding linux geckofx-winforms 60.0.26 32bit (from nuget)"
+#cp $gecko_linux/Geckofx-Winforms.dll bin/LinuxRelease/Geckofx-Winforms.dll
+#cp $gecko_linux/Geckofx-Winforms.dll.mdb bin/LinuxRelease/geckofx-Winforms.dll.mdb
 
 echo "done"
