278dbe228f206d9057b375609ed3dcc08ff884c9
narrakas <zixing.narrakas1996@gmail.com>
2019-03-15 20:40:21 -0300

    Added Docs to Netlenium/Types/Platform.c

--- Netlenium/Types/Platform.cs
+++ Netlenium/Types/Platform.cs
@@ -5,9 +5,24 @@
     /// </summary>
     public enum Platform
     {
+        /// <summary>
+        /// Auto-Detects the Platform that the system is running on
+        /// </summary>
         AutoDetect = 0,
+        
+        /// <summary>
+        /// Windows 32bit Platform
+        /// </summary>
         Win32 = 1,
+        
+        /// <summary>
+        /// Linux/Unix 32bit Platform
+        /// </summary>
         Linux32 = 2,
+        
+        /// <summary>
+        /// Linux/Unix 64bit Platform
+        /// </summary>
         Linux64 = 3
     }
 }
