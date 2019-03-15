d8574e16c8f8b6aae8d72ea3877d2e3ba0b6d4f9
narrakas <zixing.narrakas1996@gmail.com>
2019-03-15 20:41:51 -0300

    Added Docs to Netlenium/Types/SearchType.c

--- Netlenium/Types/SearchType.cs
+++ Netlenium/Types/SearchType.cs
@@ -1,11 +1,33 @@
 ﻿namespace Netlenium.Types
 {
+    /// <summary>
+    /// Search Type for Getting Elements from the active DOM
+    /// </summary>
     public enum SearchType
     {
+        /// <summary>
+        /// Gets Elements by Id
+        /// </summary>
         Id = 0,
+        
+        /// <summary>
+        /// Gets Elements by ClassName
+        /// </summary>
         ClassName = 1,
+        
+        /// <summary>
+        /// Gets Elements using CssSelector (Selenium Only)
+        /// </summary>
         CssSelector = 2,
+        
+        /// <summary>
+        /// Gets Elements using the TagName
+        /// </summary>
         TagName = 3,
+        
+        /// <summary>
+        /// Gets Elements by Name
+        /// </summary>
         Name = 4
     }
 }
