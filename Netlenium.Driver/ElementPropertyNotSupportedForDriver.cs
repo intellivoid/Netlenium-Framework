baa39fe9554e87a52f73412db701c0387f3c4dcb
narrakas <zixing.narrakas1996@gmail.com>
2019-03-16 13:42:12 -0300

    Added Missing Docs to Netlenium.Driver/ElementPropertyNotSupportedForDriver.c

--- Netlenium.Driver/ElementPropertyNotSupportedForDriver.cs
+++ Netlenium.Driver/ElementPropertyNotSupportedForDriver.cs
@@ -3,21 +3,41 @@ using System.Runtime.Serialization;
 
 namespace Netlenium.Driver
 {
+    /// <summary>
+    /// Raises when the Element Property is not supported for the selected driver
+    /// </summary>
     [Serializable]
     internal class ElementPropertyNotSupportedForDriver : Exception
     {
+        /// <summary>
+        /// Public Constructor without any context
+        /// </summary>
         public ElementPropertyNotSupportedForDriver()
         {
         }
 
+        /// <summary>
+        /// Public Constructor with Message
+        /// </summary>
+        /// <param name="message"></param>
         public ElementPropertyNotSupportedForDriver(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Public Constructor with Inner Exception stack trace
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
         public ElementPropertyNotSupportedForDriver(string message, Exception innerException) : base(message, innerException)
         {
         }
 
+        /// <summary>
+        /// Public Cos tructor for Serialized Objects
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         protected ElementPropertyNotSupportedForDriver(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
