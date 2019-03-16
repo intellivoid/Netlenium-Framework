78f6e88132d139bd5f059ba1ffa829e2cd6836d3
narrakas <zixing.narrakas1996@gmail.com>
2019-03-16 13:23:05 -0300

    Added missing docs to Netlenium.Driver/ElementMethodNotSupportedForDriver.c

--- Netlenium.Driver/ElementMethodNotSupportedForDriver.cs
+++ Netlenium.Driver/ElementMethodNotSupportedForDriver.cs
@@ -3,21 +3,41 @@ using System.Runtime.Serialization;
 
 namespace Netlenium.Driver
 {
+    /// <summary>
+    /// Raised when the element type is not supported for the selected driver
+    /// </summary>
     [Serializable]
     internal class ElementMethodNotSupportedForDriver : Exception
     {
+        /// <summary>
+        /// Public Constructor without any context
+        /// </summary>
         public ElementMethodNotSupportedForDriver()
         {
         }
 
+        /// <summary>
+        /// Public Constructor with Message
+        /// </summary>
+        /// <param name="message"></param>
         public ElementMethodNotSupportedForDriver(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Public Constructor with Inner Exception stack trace
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
         public ElementMethodNotSupportedForDriver(string message, Exception innerException) : base(message, innerException)
         {
         }
 
+        /// <summary>
+        /// Public Cos tructor for Serialized Objects
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         protected ElementMethodNotSupportedForDriver(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
