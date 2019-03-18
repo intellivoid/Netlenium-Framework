d46603f32d144c6dc4412c2c5dea46bae27b017b
narrakas <zixing.narrakas1996@gmail.com>
2019-03-17 22:24:14 -0300

    Add Netlenium.WebAPI.Google to the Framework assets

---
+++ Netlenium.WebAPI.Google/ChromeDriver/Content.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Netlenium.WebAPI.Google.ChromeDriver
+{
+    public class Content
+    {
+        public string Name { get; set; }
+        
+        public int Generation { get; set; }
+        
+        public int MetaGeneration { get; set; }
+        
+        public string LastModified { get; set; }
+        
+        public string ETag { get; set; }
+        
+        public int Size { get; set; }
+        
+        public Uri AccessLocation { get; set; }
+    }
+}
---
+++ Netlenium.WebAPI.Google/ChromeDriver/Storage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net;
+using System.Xml;
+
+namespace Netlenium.WebAPI.Google.ChromeDriver
+{
+    public class Storage
+    {
+        /// <summary>
+        /// Fetches the content from the Storage API
+        /// </summary>
+        /// <param name="endPoint"></param>
+        /// <returns></returns>
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        public static List<Content> FetchContents(string endPoint = "https://chromedriver.storage.googleapis.com/")
+        {
+            var httpClient = new WebClient();
+            var response = httpClient.DownloadString(endPoint);
+            
+            var xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(response);
+
+            return(from XmlNode contentNode in xmlDocument.SelectNodes("ListBucketResult/Contents")
+                select new Content
+                {
+                    Name = contentNode.SelectSingleNode("//Key").Value,
+                    Generation = Convert.ToInt32(contentNode.SelectSingleNode("//Generation").Value),
+                    MetaGeneration = Convert.ToInt32(contentNode.SelectSingleNode("//MetaGeneration").Value),
+                    LastModified = contentNode.SelectSingleNode("//LastModified").Value,
+                    ETag = contentNode.SelectSingleNode("//ETag").Value,
+                    Size = Convert.ToInt32(contentNode.SelectSingleNode("//Size").Value),
+                    AccessLocation = new Uri($"{endPoint}{contentNode.SelectSingleNode("//Key").Value}");
+                }).ToList();
+        }
+    }
+}
