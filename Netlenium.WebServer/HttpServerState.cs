5393b7ff24f0901376f1a0c2eb98c5a4a357ea1c
Narrakas <zixing.narrakas1996@gmail.com>
2019-03-27 11:44:56 -0300

    Added Netlenium.WebServer

---
+++ Netlenium.WebServer/HttpServerState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Netlenium.WebServer
+{
+    public enum HttpServerState
+    {
+        Stopped,
+        Starting,
+        Started,
+        Stopping
+    }
+}
