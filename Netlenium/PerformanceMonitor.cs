38d3175cd07c7b9ff01c2ad39281066524573488
Narrakas <zixing.narrakas1996@gmail.com>
2019-03-29 21:06:45 -0300

    Added docs to Netlenium/PerformanceMonitor.c

--- Netlenium/PerformanceMonitor.cs
+++ Netlenium/PerformanceMonitor.cs
@@ -1,18 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace Netlenium
 {
+    /// <summary>
+    /// Performance Monitor Class which monitors CPU/RAM usage of a process
+    /// </summary>
     public class PerformanceMonitor
     {
+        /// <summary>
+        /// The Memory Counter
+        /// </summary>
         private PerformanceCounter MemoryCounter { get; set; }
 
+        /// <summary>
+        /// The CPU Counter
+        /// </summary>
         private PerformanceCounter CpuCounter { get; set; }
 
+        /// <summary>
+        /// Returns the Memory Usage in MB
+        /// </summary>
         public double MemoryUsage
         {
             get
@@ -21,6 +28,9 @@ namespace Netlenium
             }
         }
 
+        /// <summary>
+        /// Returns the CPU Usage (0-100)
+        /// </summary>
         public double CpuUsage
         {
             get
@@ -29,12 +39,21 @@ namespace Netlenium
             }
         }
 
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="processId"></param>
         public PerformanceMonitor(int processId)
         {
             MemoryCounter = new PerformanceCounter("Process", "Working Set", GetProcessInstanceName(processId));
             CpuCounter = new PerformanceCounter("Process", "% Processor Time", GetProcessInstanceName(processId));
         }
 
+        /// <summary>
+        /// Gets the process instance name by Process ID
+        /// </summary>
+        /// <param name="pid"></param>
+        /// <returns></returns>
         private static string GetProcessInstanceName(int pid)
         {
             PerformanceCounterCategory cat = new PerformanceCounterCategory("Process");
